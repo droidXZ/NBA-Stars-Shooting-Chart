d5d7bd0b2c32f0cce3ffc887d688333e7f6ab1bf
wangxin <17656115@qq.com>
2017-11-28 20:40:48 +0800

    selelct player

--- NBAStarShootingVisPrj/js/index.js
+++ NBAStarShootingVisPrj/js/index.js
@@ -3,10 +3,13 @@
   // 当前默认的球星和球星Id 检索可通过名称 绘制均通过Id进行查找数据
   var currentStar = "Russell Westbrook",
       currentStarId = 201566;
-  document.getElementsByClassName('buttonBox')[0].onclick = function (){
+  document.getElementsByClassName('buttonBox')[0].onclick = function(){
     document.getElementsByClassName('selectPlayer')[0].style.display = 'block';
   }
-
+  document.getElementById('hideButton').onclick = function(){
+    document.getElementsByClassName('selectPlayer')[0].style.display = 'none';
+  }
+  
 d3.json("json/nbaStarData2016-2017.json",function(data){
   console.log(currentStar + currentStarId);
   console.log(data);
