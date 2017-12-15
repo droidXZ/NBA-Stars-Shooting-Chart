fdda0264b08dc293af6bfdd16734901da5ead3fa
wangxin <17656115@qq.com>
2017-12-15 19:56:40 +0800

    修改一些样式

--- NBAStarShootingVisPrj/js/compare.js
+++ NBAStarShootingVisPrj/js/compare.js
@@ -3,67 +3,15 @@
   // 当前默认的球星和球星Id 检索可通过名称 绘制均通过Id进行查找数据
   var currentStar = "Russell Westbrook",
       currentStarId = 201566;
-
-  var currentStarR = "Stephen Curry",
-      currentStarIdR = 201939;
-  // 判断点击change按钮次数，只加载一次列表
-  var clickTimes = 0;
+  var FDSTR = 'all';
+  var CHANGEEVENT = 0;
+  var SHOTDATA = [];
   bindStarData();
   document.getElementsByClassName('buttonBox')[0].onclick = function(){
     document.getElementsByClassName('selectPlayer')[0].style.display = 'block';
-    // 绑定搜索框球星列表
-    if(clickTimes === 0){
-      bindSerachImg();
-      clickTimes++;
-    }
     //通过点击图片更改当前currentId
     d3.selectAll(".starPicSelect")
       .on("click",function(d){
-        alert(d3.select(this));
-        var cDiv = d3.select(this);
-        console.log(cDiv);
-        //取得点击图片的球星ID和name
-        var idStr = cDiv[0][0].firstChild.getAttribute("src"),
-            nameStr = cDiv[0][0].lastChild.innerHTML;
-        idStr = idStr.slice(11);
-        idStr = idStr.substr(0,idStr.indexOf('.'));
-
-        changeCurrentStar(idStr,nameStr);
-        //点击切换球星后 隐藏选择框
-
-        document.getElementsByClassName('selectPlayer')[0].style.display = 'none';
-        resetSerachPic();
-      });
-    // 点击搜索 进行搜索
-    document.getElementById("serachButton").onclick = function(){
-      var serachStr = document.getElementById("inputV").value.toLowerCase();
-      var cDivBox = document.getElementsByClassName("starPicSelect");
-      for(var i in cDivBox){
-        var idStr = cDivBox[i].firstChild.getAttribute("src"),
-            nameStr = cDivBox[i].lastChild.innerHTML;
-        idStr = idStr.slice(11);
-        idStr = idStr.substr(0,idStr.indexOf('.'));
-        if(serachStr == nameStr.toLowerCase() || serachStr == idStr){
-          continue;
-        }else {
-          cDivBox[i].style.display = "none";
-        }
-      }
-    }
-  }
-
-  //右邊的球星
-  document.getElementsByClassName('buttonBoxR')[0].onclick = function(){
-    document.getElementsByClassName('selectPlayer')[0].style.display = 'block';
-    // 绑定搜索框球星列表
-    if(clickTimes === 0){
-      bindSerachImg();
-      clickTimes++;
-    }
-    //通过点击图片更改当前currentId
-    d3.selectAll(".starPicSelect")
-      .on("click",function(d){
-        alert(d3.select(this));
         var cDiv = d3.select(this);
         console.log(cDiv);
         //取得点击图片的球星ID和name
@@ -99,8 +47,10 @@
   document.getElementById('hideButton').onclick = function(){
     document.getElementsByClassName('selectPlayer')[0].style.display = 'none';
     resetSerachPic();
-}
-
+  }
+  document.getElementById("filterSelect").onchange = function(){
+    changeShotDataToDraw(this.value);
+  };
   // 绑定球星数据。
   function bindStarData(){
     d3.json("json/nbaStarData2016-2017.json",function(data){
@@ -128,7 +78,6 @@
       }
     });
   }
-  
 //将球星数据库中的数据绑定到serachStarPicBox中去
  function bindSerachImg(){
    d3.json("json/nbaStarData2016-2017.json",function(data){
@@ -160,6 +109,7 @@
 
   //改变当前球星id和球星名称
   function changeCurrentStar(idStr,nameStr){
+    CHANGEEVENT = 0;
     currentStar = nameStr;
     currentStarId = Number(idStr);
     // 在下面调用绘制函数更改当前的信息及图片
